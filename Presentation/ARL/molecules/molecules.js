edc8c98a131496e2170f8a6ea0a906c3567f1983
Kevin Waters <kwaters@mtu.edu>
2018-03-16 13:51:10 -0700

    Modify Protein jmol size, part 5, end all with ;

--- Presentation/ARL/molecules/molecules.js
+++ Presentation/ARL/molecules/molecules.js
@@ -182,12 +182,12 @@
         $(document).ready(function() {
         //  Jmol.getApplet("jmolApplet0", Info)
         //  $("#appdiv").html(Jmol.getAppletHtml("jmolApplet0"))
-          $("#appdiv1").html(Jmol.getAppletHtml("jmolApplet0", Info))
-          $("#appdiv2").html(Jmol.getAppletHtml("jmolApplet1", Info2))
-          $("#appdiv3").html(Jmol.getAppletHtml("jmolApplet2", Info3))
-          $("#appdiv4").html(Jmol.getAppletHtml("jmolApplet3", Info4))
-          $("#appdiv5").html(Jmol.getAppletHtml("jmolApplet4", Info5))
+          $("#appdiv1").html(Jmol.getAppletHtml("jmolApplet0", Info));
           $("#appdiv1").html(Jmol.resizeApplet("jmolApplet0", [500,800]));
-        })
+          $("#appdiv2").html(Jmol.getAppletHtml("jmolApplet1", Info2));
+          $("#appdiv3").html(Jmol.getAppletHtml("jmolApplet2", Info3));
+          $("#appdiv4").html(Jmol.getAppletHtml("jmolApplet3", Info4));
+          $("#appdiv5").html(Jmol.getAppletHtml("jmolApplet4", Info5));
+        });
 
         var lastPrompt=0;
--- index.md
+++ index.md
@@ -19,5 +19,3 @@ There are many elements of the site that are still under construction, but feel 
 
 
 
-
-
