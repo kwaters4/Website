19f7989300d48fc04c5202062b3767601948a6d1
Kevin Waters <kwaters@mtu.edu>
2018-03-16 12:47:57 -0700

    Added gold figures with references, testing table for overview

--- Presentation/ARL/molecules/molecules.js
+++ Presentation/ARL/molecules/molecules.js
@@ -25,8 +25,8 @@
         //}
 
         var Info = {
-            width: 100%,
-            height: 100%,
+            width: 2000px,
+            height: 2000px,
             debug: false,
             color: "transparent",
         //  color: "#111111",
