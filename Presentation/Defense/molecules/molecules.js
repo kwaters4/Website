cdef95c22f9cb12a0a2e621163be41fc9c280f55
Kevin Waters <kwaters@mtu.edu>
2018-07-09 11:01:40 -0400

    Intro continuation

--- Presentation/Defense/molecules/molecules.js
+++ Presentation/Defense/molecules/molecules.js
@@ -188,6 +188,37 @@
             //defaultModel: "$dopamine",
             //console: "none", // default will be jmolApplet0_infodiv, but you can designate another div here or "none"
         }
+        var Info6 = {
+            width: 500,
+            height: 500,
+            debug: false,
+            color: "transparent",
+        //  color: "#111111",
+        //        coverImage: "1fss.png",
+        //        coverTitle: "TEST",
+        //        deferApplet: true,
+        //        deferUncover: true,
+        //  addSelectionOptions: true,
+            use: "HTML5",   // JAVA HTML5 WEBGL are all options
+            j2sPath: "plugin/jsmol/j2s", // this needs to point to where the j2s directory is.
+            jarPath: "plugin/jsmol/java",// this needs to point to where the java directory is.
+            jarFile: "JmolAppletSigned.jar",
+            isSigned: true,
+        //  script: "set antialiasDisplay; set zoomLarge true; load jsmol/data/caffeine.mol",
+            script: "background white; set antialiasDisplay; load molecules/BN_88.xyz; set frank off",
+        //        scr: 'test.spt',
+        //        script: "set zoomLarge true",
+        //  serverURL: "http://chemapps.stolaf.edu/jmol/jsmol/php/jsmol.php",
+            readyFunction: jmol_isReady,
+            disableJ2SLoadMonitor: true,
+                disableInitialConsole: true,
+                allowJavaScript: true,
+                antialiasDisplay: true,
+        //        script: "display not water",
+        //        zoomScaling: 4.0
+            //defaultModel: "$dopamine",
+            //console: "none", // default will be jmolApplet0_infodiv, but you can designate another div here or "none"
+        }
         var jmolcmds = [
         "set frank on",
         ]
@@ -200,6 +231,7 @@
           $("#appdiv3").html(Jmol.getAppletHtml("jmolApplet2", Info3))
           $("#appdiv4").html(Jmol.getAppletHtml("jmolApplet3", Info4))
           $("#appdiv5").html(Jmol.getAppletHtml("jmolApplet4", Info5))
+          $("#appdiv6").html(Jmol.getAppletHtml("jmolApplet5", Info6))
           $("#jsmolbtnss").html(Jmol.jmolButton("jmolApplet0","select *;color chains; cartoon only;" ,"Peptide Chains") + Jmol.jmolButton("jmolApplet0", "select *; wireframe 0.15; spacefill 23%; cartoon off;","Atoms"))
           $("#jsmolbtnsc").html(Jmol.jmolButton("jmolApplet0", "select *; color cpk;","Elemental") + Jmol.jmolButton("jmolApplet0","select *;color amino" ,"Amino Acids"))
           $("#jmolspin").html(Jmol.jmolButton("jmolApplet0", "select *; color cpk;","Elemental") + Jmol.jmolButton("jmolApplet0","select *;color amino" ,"Amino Acids"))
